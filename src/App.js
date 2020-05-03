31ddef7c93c4d1c3a20b1c7bbd75a1d2391bd784
vladimir <vladim.volovenko@gmail.com>
2020-05-03 21:20:14 +0300

    React.lazy и React.Suspense, зачем? #94 - ReactJS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/nav/Navbar";
-import ProfileContainer from "./components/content/profileContainer";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import { Route, withRouter } from "react-router-dom";
-import Musik from "./components/musik/musik";
-import News from "./components/news/news";
-import Settings from "./components/settings/settings";
 import UsersContainer from "./components/users/usersContainer";
 import HeaderContainer from "./components/header/headerContainer";
-import Login from './components/login/login'
 import { connect } from "react-redux";
 import { initializeApp } from './components/redux/app-reducer'
 import { compose } from "redux";
 import Preloader from "./components/common/preload/preload";
+// import ProfileContainer from "./components/content/profileContainer";
+const ProfileContainer = React.lazy(() => import('./components/content/profileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const Musik = React.lazy(() => import('./components/musik/musik'));
+const News = React.lazy(() => import('./components/news/news'));
+const Settings = React.lazy(() => import('./components/settings/settings'));
+const Login = React.lazy(() => import('./components/login/login'));
+
 
 class App extends React.Component {
   // debugger;
@@ -31,6 +33,7 @@ class App extends React.Component {
         <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
+        <React.Suspense fallback={<div>Loading...</div>}>
           <Route
             path="/dialogs"
             render={() => <DialogsContainer />}
@@ -46,6 +49,7 @@ class App extends React.Component {
           <Route path="/news" component={News} />
           <Route path="/musik" component={Musik} />
           <Route path="/settings" component={Settings} />
+          </React.Suspense>
         </div>
     </div>
   );
